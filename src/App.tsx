fcf2da03acd944e0bdb84e123cedebd8d047ebb9
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-02 17:44:20 +0900

    chore(react-router): React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
-import reactLogo from './assets/react.svg';
-import './App.css';
+import Root from './routes/Root';
 
 const queryClient = new QueryClient();
 
-function App() {
-  const [count, setCount] = useState(0);
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root />} />));
 
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </div>
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
