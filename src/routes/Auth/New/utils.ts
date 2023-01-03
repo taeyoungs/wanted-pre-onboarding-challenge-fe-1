f808bbb3c74f85ce353d2718443f4685a49508ba
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-03 13:43:43 +0900

    feat(signup): 회원가입 페이지 및 action 함수 구현

---
+++ src/routes/Auth/New/utils.ts
@@ -0,0 +1,39 @@
+import { ActionFunctionArgs, redirect } from 'react-router-dom';
+
+import signUp from 'api/auth/signup';
+
+type SignUpParameters = Parameters<typeof signUp>[0];
+
+function isSignUpParameters(args: any): args is SignUpParameters {
+  const hasEmail = args.email !== undefined;
+  const hasPassword = args.password !== undefined;
+
+  return hasEmail && hasPassword;
+}
+
+const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const formDataEntries = Object.fromEntries(formData);
+
+  if (!isSignUpParameters(formDataEntries)) {
+    throw new Error('Incorrect name mapping of html tag.');
+  }
+
+  try {
+    const { token, message } = await signUp(formDataEntries);
+
+    if (!token) {
+      alert('토큰 정보가 유요하지 않습니다.');
+      return redirect('/auth/create');
+    }
+
+    localStorage.setItem('token', token);
+    alert(message);
+    return redirect('/');
+  } catch (error) {
+    alert('회원가입에 실패했습니다.');
+    return redirect('/auth/create');
+  }
+};
+
+export { action };
