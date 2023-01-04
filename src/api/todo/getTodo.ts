0fccb73d054b8bb7e3f4756c47c8479b11afdc1c
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-04 20:55:40 +0900

    feat(getTodo): 단일 Todo 조회 API 요청 함수 추가

---
+++ src/api/todo/getTodo.ts
@@ -0,0 +1,23 @@
+import { QueryFunctionContext } from '@tanstack/react-query';
+
+import { axiosInstance } from 'api';
+import { todoKeys } from 'factory';
+
+import type { IResponseBody, ITodo } from 'api/types';
+
+const getTodo = async ({
+  queryKey,
+}: QueryFunctionContext<ReturnType<typeof todoKeys['detail']>>) => {
+  const [{ id }] = queryKey;
+  const token = localStorage.getItem('token');
+
+  return axiosInstance
+    .get<IResponseBody<ITodo>>(`/todos/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((res) => res.data.data);
+};
+
+export default getTodo;
